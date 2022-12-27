48610f7bf5b3d1f799fb4bd001ee385a6bc3b7fc
Anna Vernadskaya <anna-vernadskaya@yandex.ru>
2022-12-27 18:07:02 +0600

    после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,14 @@ const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 //выбираем кнопку, открывающую попап на странице
 const popupOpenButtonElement = document.querySelector('.profile__button-edit');
+// Находим форму в DOM
+const formElement = document.querySelector('.popup__form');
+// Находим поля формы в DOM
+const usernameInput = formElement.querySelector('.popup__input_form_username');
+const jobInput = formElement.querySelector('.popup__input_form_aboutme');
+// Выберите элементы, куда должны быть вставлены значения полей
+const profileName = document.querySelector('.profile__name');
+const profileAboutMe = document.querySelector('.profile__about-me');
 
 //добавляем модификатор в html
 const openPopup = function() {
@@ -18,20 +26,7 @@ const closePopup = function() {
   popupElement.classList.remove('popup_is-opened');
 }
 
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
-
-
 //СОХРАНЕНИЕ В ФОРМЕ
-// Находим форму в DOM
-const formElement = document.querySelector('.popup__form');
-// Находим поля формы в DOM
-const usernameInput = formElement.querySelector('.popup__form-username');
-const jobInput = formElement.querySelector('.popup__form-about-me');
-// Выберите элементы, куда должны быть вставлены значения полей
-const profileName = document.querySelector('.profile__name');
-const profileAboutMe = document.querySelector('.profile__about-me');
-
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -40,10 +35,6 @@ function handleFormSubmit (evt) {
 // Так мы можем определить свою логику отправки.
 
     // Получите значение полей jobInput и nameInput из свойства value
-    usernameInput.value;
-    jobInput.value;
-
-
     // Вставьте новые значения с помощью textContent
     profileName.textContent = usernameInput.value;
     profileAboutMe.textContent = jobInput.value;
@@ -53,4 +44,7 @@ function handleFormSubmit (evt) {
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
+
